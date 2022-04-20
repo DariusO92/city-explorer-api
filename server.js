2b74c19fec82a2ebb621d7808af5c652e2f061c4
Darius <darius.owens.j@gmail.com>
2022-04-19 22:35:25 -0700

    started lab 7

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// requires
+const express = require('express');
+require('dotenv').config();
+
+// use 
+
+
+const app = express();
+
+// define PORT and Validate
+const PORT = process.env.PORT || 3002;
+
+
+// ROUTES
+// will use these for endpoint
+
+app.get('/')
+
+// errors
+
+// Listen
+// Start the server
+// listen is an express method
